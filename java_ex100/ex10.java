32e95c9ad921d14583250abed3943a3e461c4241
KimMunho <74893339+KimMunho@users.noreply.github.com>
2023-11-07 17:50:35 +0900

    수정

---
+++ java_ex100/ex10.java
@@ -0,0 +1,17 @@
+package java_ex100;
+import java.util.Scanner;
+
+public class ex10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.print(num1);
+		System.out.print(num2);
+		sc.close();
+
+	}
+
+}
